19a633f7e7427c51a4324bf67824ba826829c91c
Murad <goysultanov.murad@gmail.com>
2026-07-08 00:45:49 +0300

    Создание лодера загрузки страницы, но пока без обратного

--- modules/api.js
+++ modules/api.js
@@ -1,7 +1,12 @@
+import { listLoader } from './init.js'
+
 export function getCommentsApi() {
     return fetch(
         'https://wedev-api.sky.pro/api/v1/murad-goysultanov/comments',
     ).then((response) => response.json())
+    // .then(() => {
+    //     listLoader()
+    // })
 }
 
 export function postCommentsApi(newComment) {
